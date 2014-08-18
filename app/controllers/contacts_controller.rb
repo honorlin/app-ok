eff8c971f67ee955871ceab92e52cfc281a7b3a5
honor <tenhon.lin@gmail.com>
2014-08-19 04:07:47 +0800

    the contacts of agnet CRUD operation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,47 @@
 class ContactsController < ApplicationController
+	before_action :find_agent
+
   def index
-  	@contacts = Agent.find(params[:agent_id]).contacts.all
+  	@contacts = @agent.contacts.all
   end
 
   def new
-  	@contact = Contact.new
-  	
+  	@contact = @agent.contacts.new
   end
 
   def show
+  	@contact = @agent.contacts.find(params[:id])
   end
 
   def edit
+		@contact = @agent.contacts.find(params[:id])
   end
 
+  def update
+  	@contact = @agent.contacts.find(params[:id])
+  	@contact.update_attributes(contacts_params)
+  	redirect_to agent_contacts_path(@agent)
+  end
+
+  def create
+  	@agent.contacts.create(contacts_params)
+  	redirect_to agent_contacts_path(@agent)
+  end
+
+  def destroy
+  	@contact = @agent.contacts.find(params[:id])
+  	@contact.destroy
+  	redirect_to agent_contacts_path(@agent)
+  end
+
+private
+
+	def find_agent
+		@agent = Agent.find(params[:agent_id])
+	end
+
+	def contacts_params
+		params.require(:contact).permit(:name, :email)		
+	end
 
 end
